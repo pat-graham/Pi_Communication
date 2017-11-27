8a5c0e2aec17d8fb741a781ae96d30c9cd372ebf
Pat <pdgra1@student.monash.edu>
2017-11-27 02:51:17 +0000

    Fixed received script errors

--- Receive/receive.py
+++ Receive/receive.py
@@ -5,8 +5,8 @@ UDP_PORT = 5005
 
 sock = socket.socket(socket.AF_INET,  # Internet
                      socket.SOCK_DGRAM)  # UDP
-sock.bind(MESSAGE, (UDP_IP, UDP_PORT))
+sock.bind((UDP_IP, UDP_PORT))
 
 while True:
-data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
-print "received message:" data
+	data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
+	print "received message:", data
